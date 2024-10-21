c071e6f26a70babd22b9fe340bc34fe0a73ccfbe
i-salman <muhammad.salman@con.pucrecs.com>
2024-10-21 21:10:49 +0500

    update

--- MyToasterLib/src/main/java/com/cloud/mytoasterlib/ToastMessage.kt
+++ MyToasterLib/src/main/java/com/cloud/mytoasterlib/ToastMessage.kt
@@ -3,8 +3,6 @@ package com.cloud.mytoasterlib
 import android.content.Context
 import android.util.Log
 import android.widget.Toast
-import com.payment.paymentsdk.PaymentSdkConfigBuilder
-import com.payment.paymentsdk.integrationmodels.PaymentSdkBillingDetails
 
 class ToastMessage {
 
@@ -16,23 +14,5 @@ class ToastMessage {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 
-    fun test(context: Context) {
-        var builder: PaymentSdkConfigBuilder
-        val billingData = PaymentSdkBillingDetails(
-            "London",
-            "Pk",
-            "abc@gmail.com",
-            "Abc",
-            "09876543321",
-            "12345",
-            "Denmark",
-            "12345"
-        )
 
-        Log.d("Toaster", "${billingData.zip}")
-        Log.d("Toaster", "${billingData.toString()}")
-        showLongToast(context, billingData.zip!!)
-        showLongToast(context, billingData.toString())
-
-    }
 }
